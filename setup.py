e8270616b700b838a14d515b427f412acdcbf5d3
Thibault Lestang <thibault.lestang@meteo.fr>
2024-02-26 17:19:51 +0100

    Replace distutils by setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="climate-aware-task-scheduler",
     version="0.1.0",
     description = "Climate aware task scheduler",
+    install_requires=[
+          "requests_cache>=1.0",
+          "PyYAML>=6.0",
+    ],
     python_requires=">=3.9",
     license_files=["LICENSE"],
+    entry_points={
+	'console_scripts': [
+	    'cats = cats:main',
+	]
+    },
     packages=["cats"],
 )
