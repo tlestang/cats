20416d9e00f831349d1028a7a6786f3ebbbaf44a
Thibault Lestang <thibault.lestang@meteo.fr>
2024-02-26 17:01:25 +0100

    Add distutils setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="climate-aware-task-scheduler",
+    version="0.1.0",
+    description = "Climate aware task scheduler",
+    python_requires=">=3.9",
+    license_files=["LICENSE"],
+    packages=["cats"],
+)
