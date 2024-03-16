3ef6e84323fdfd5b02d49a63eee86bb188207211
Thibault Lestang <thibault.lestang@protonmail.com>
2024-03-16 08:02:17 +0100

    partition->profile and make it optional

--- cats/check_clean_arguments.py
+++ cats/check_clean_arguments.py
@@ -2,10 +2,10 @@ import re
 import sys
 
 
-def validate_jobinfo(jobinfo: str, expected_partition_names):
+def validate_jobinfo(jobinfo: str, expected_profile_names):
     """Parses a string of job info keys in the form
 
-    partition=CPU_partition,memory=8,ncpus=8,ngpus=0
+    profile=GPU_partition,memory=8,ncpus=8,ngpus=4
 
     and checks all required info keys are present and of the right type.
 
@@ -17,27 +17,27 @@ def validate_jobinfo(jobinfo: str, expected_partition_names):
     """
 
     expected_info_keys = (
-        "partition",
         "memory",
         "cpus",
         "gpus",
     )
-    info = dict([match.groups() for match in re.finditer(r"(\w+)=(\w+)", jobinfo)])
+    info = dict([m.groups() for m in re.finditer(r"(\w+)=(\w+)", jobinfo)])
 
     # Check if some information is missing
     if missing_keys := set(expected_info_keys) - set(info.keys()):
         sys.stderr.write(f"ERROR: Missing job info keys: {missing_keys}")
         return {}
 
+    profile = info.pop("profile", "default")
     # Validate partition value
-    if info["partition"] not in expected_partition_names:
+    if profile not in expected_profile_names:
         sys.stderr.write(
-            f"ERROR: job info key 'partition' should be one of {expected_partition_names}. Typo?\n"
+            f"ERROR: job info key 'profile' should be one of {expected_profile_names}. Typo?\n"
         )
         return {}
 
     # check that `cpus`, `gpus` and `memory` are numeric and convert to int
-    for key in [k for k in info if k != "partition"]:
+    for key in [k for k in info]:
         try:
             info[key] = int(info[key])
         except ValueError:
